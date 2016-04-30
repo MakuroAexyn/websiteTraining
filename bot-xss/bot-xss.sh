1e4c4f71fb02c8a498aca95d1876b02b8280e388
Ektoplasma <acrasidae@gmail.com>
2016-04-30 18:50:26 +0200

    bot fonctionnel, ajout commande cron

--- bot-xss/bot-xss.sh
+++ bot-xss/bot-xss.sh
@@ -1,1 +1,1 @@
-while sleep 5; do phantomjs --ignore-ssl-errors=true --local-to-remote-url-access=true --web-security=false --ssl-protocol=any xss-bot.js; done;
+while sleep 5; do /root/phantomjs-2.1.1-linux-x86_64/bin/phantomjs --ignore-ssl-errors=true --local-to-remote-url-access=true --web-security=false --ssl-protocol=any /usr/bot-xss/xss-bot.js; done;
