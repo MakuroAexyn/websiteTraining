ae45f95d0bcc2f9bd5a78bd890535df4bee2a1a4
Ektoplasma <acrasidae@gmail.com>
2016-05-01 10:51:22 +0200

    fix merge

--- pages/liste_clients.php
+++ pages/liste_clients.php
@@ -13,11 +13,9 @@
 </head>
 
 <body>
-<<<<<<< HEAD
 
         <!-- insert the page content here -->
-=======
->>>>>>> 0d652711dd2de80b17f8e112a473485183e7b3af
+
         <h1>Les membres de l'association ISI </h1>
   
 			<ul>
@@ -25,10 +23,6 @@
 			  <li><h4>Jeanne </h4></li>
 			  <li><h4>Pablo </h4></li>
 			</ul>
-			
-<<<<<<< HEAD
-=======
-			
->>>>>>> 0d652711dd2de80b17f8e112a473485183e7b3af
+
 </body>
 </html>
